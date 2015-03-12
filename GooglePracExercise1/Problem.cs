8cef858f42806ca5a7aaa78011ea2cf3e1d38c85
Ryan Fore <ryan.b.fore@gmail.com>
2015-03-12 16:19:51 -0500

    Initial commit

---
+++ GooglePracExercise1/Problem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GooglePracExercise1
+{
+    class Problem
+    {
+        public int NumberOfSwitches { get; set; }
+        public int NumberOfOutlets { get; set; }
+        public List<string> currentState { get; set; }
+        public List<string> neededState { get; set; }
+
+        public void Run()
+        {
+            foreach (var curr in currentState)
+            {
+                var not = Not(curr);
+            }
+        }
+
+        private string Not(string inString)
+        {
+            List<char> newStringList = new List<char>();
+            foreach(var currChar in inString)
+            {
+                if (currChar == '0')
+                {
+                    newStringList.Add('1');
+                }
+
+                if (currChar == '1')
+                {
+                    newStringList.Add('0');
+                }
+            }
+
+            return new string(newStringList.ToArray());
+        }
+    }
+}
---
+++ GooglePracExercise1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GooglePracExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Problem> problemList = new List<Problem>();
+            using (var stream = new StreamReader(new FileStream(@"C:\temp\Google\A-small-practice.in", FileMode.Open)))
+            {
+                var numCases = int.Parse(stream.ReadLine());
+
+                for (int i = 0; i < numCases; ++i)
+                {
+                    var prob = new Problem();
+                    var caseInfo = stream.ReadLine().Split(' ');
+
+                    prob.NumberOfOutlets = int.Parse(caseInfo[0]);
+                    prob.NumberOfSwitches = int.Parse(caseInfo[1]);
+
+                    prob.currentState = stream.ReadLine().Split(' ').ToList();
+                    prob.neededState = stream.ReadLine().Split(' ').ToList();
+
+                    problemList.Add(prob);
+                }
+            }
+
+            foreach (var pro in problemList)
+            {
+                pro.Run();
+            }
+
+            var ere = 1;
+        }
+    }
+}
